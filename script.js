4c1fc310947b6dd80e0b7495b30aa561f11f34b7
Yash <yashr3712@gmail.com>
2026-03-02 20:57:10 +0530

    Replay

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const msgEl = document.getElementById("msg-el");
 function player(){
     console.log("Hello World")
-    msgEl.textContent = "Hmm, pata tha mujhe :)";
+    msgEl.textContent = "Hmm, pata tha mujhe :) (Refresh to play again)";
 };
 
 function playerYes(){
     console.log("Hello World")
     msgEl.textContent = "Wow, you are intelligent :)";
+
 };
