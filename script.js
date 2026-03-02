fe73d7cc2ca1f3e1f48c37c8e30963a6d801462c
Yash <yashr3712@gmail.com>
2026-03-02 20:53:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const msgEl = document.getElementById("msg-el");
+function player(){
+    console.log("Hello World")
+    msgEl.textContent = "Hmm, pata tha mujhe :)";
+};
+
+function playerYes(){
+    console.log("Hello World")
+    msgEl.textContent = "Wow, you are intelligent :)";
+};
